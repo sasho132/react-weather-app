7703b35c10cf137780773983551590e867f1bef4
Aleksandar Petrov <sasho1320@gmail.com>
2023-05-14 12:00:24 +0300

    Update search.js and add api.js file

---
+++ src/components/api.js
@@ -0,0 +1,8 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0939baa7c5mshff0ef06f21a5106p1c313cjsn58819f3b2fc6',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
