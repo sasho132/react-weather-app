d4cfe5f4e389127ab9d23dd54b4e1f8e3686bfb3
Aleksandar Petrov <sasho1320@gmail.com>
2023-05-15 15:29:20 +0300

    Update api.js and App.js files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_KEY, WEATHER_API_URL } from './components/api';
 
 function App() {
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [ lat, lon ] = searchData.split(", ");
+
+    const currentWeather = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+    
+    const forecast = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
   }
  
   return (
--- src/components/api.js
+++ src/components/api.js
@@ -2,7 +2,10 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '0939baa7c5mshff0ef06f21a5106p1c313cjsn58819f3b2fc6',
+		'X-RapidAPI-Key': (process.env.REACT_APP_RAPID_API_KEY),
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = (process.env.REACT_APP_WEATHER_API_KEY);
